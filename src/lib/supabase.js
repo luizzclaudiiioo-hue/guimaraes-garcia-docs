e5a1909b42fa570e59e4272d5b23e6e3a6a17db3
luizzclaudiiioo-hue <luizzclaudiiioo@gmail.com>
2026-05-15 04:51:13 -0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const SUPABASE_URL = 'https://ybewjqkaeyslbzujcpyz.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_WH1_aAVryPEcanp7UUgsKw_OWyLoRsc'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
